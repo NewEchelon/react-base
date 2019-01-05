1bdbba1b5c1e2ae922c72deddd03c976653b1c60
newechelon <newechelondesigns@gmail.com>
2019-01-05 16:32:45 -0700

    updating file structure

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+const appRoot = document.getElementById('app');
+ReactDOM.render(<p>This is my boilerplate</p>, appRoot)
+
+
+
